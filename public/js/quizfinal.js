b2c6558be8dd6cf18de6ac9a5d4513d109a8d501
Rohit <rohitkul247@gmail.com>
2021-02-02 11:59:37 +0530

    added js files

---
+++ public/js/quizfinal.js
@@ -0,0 +1,29 @@
+function countDown(sec, elem){
+    var element = document.getElementById(elem);
+    element.innerHTML=sec;
+    if(sec<1){
+        clearTimeout(timer);
+        document.getElementById('countdown').innerHTML="0"
+        // element.innerHTML='<h1 style="padding:0px;font-size:59px">Time Up!</h1>';
+    }
+    sec--;
+    var timer =setTimeout('countDown('+sec+',"'+elem+'")', 1000 );
+}
+
+$(document).ready(function() {
+    $('#question').bind('copy paste cut',function(e) { 
+        e.preventDefault(); //disable cut,copy,paste
+        
+    });
+});
+
+$(document).ready(function() {
+    $('#field').bind('copy paste cut',function(e) { 
+        e.preventDefault(); //disable cut,copy,paste
+        
+    });
+});
+
+// $(window).blur(function() {
+//     window.location="/logout";
+// });
