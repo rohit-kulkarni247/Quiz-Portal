0c03b1c9ed1c8c0ce34e9a44b4e06df8c5586285
kausko <kausko@users.noreply.github.com>
2021-02-12 02:07:48 +0530

    Fixed instr, quizfinal, complete

--- app.js
+++ app.js
@@ -331,7 +331,7 @@ app.post('/', async function (req, res, next) {
 
   if (quiztype == 'melaquiz') {
     if (currentDate == 12) {
-      if (currentHrs == 00) {
+      if (currentHrs == 2) {
         if (currentMin >= 0 && currentMin <= 59) {
           await axios.post('https://backend.credenz.in/eventlogin', {
             username: username,
