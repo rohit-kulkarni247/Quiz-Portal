df5ccf9771426e412c426ec0ef7427151ef8153a
Rohit <rohitkul247@gmail.com>
2021-02-13 13:30:27 +0530

    new changes

--- app.js
+++ app.js
@@ -415,8 +415,8 @@ app.post('/', async function (req, res, next) {
   }
   else if (quiztype == 'generalquiz') {
     if (currentDate == 13) {
-      if (currentHrs == 12) {
-        if (currentMin >= 0 && currentMin <= 30) {
+      if (currentHrs == 13) {
+        if (currentMin >= 0 && currentMin <= 59) {
           await axios.post('https://backend.credenz.in/eventlogin', {
             username: username,
             event: quiztype,
